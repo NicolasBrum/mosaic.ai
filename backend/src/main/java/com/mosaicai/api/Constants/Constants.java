eee52f8af42f3ad5c688da8e2d65d5eae0cb2d54
NicolasBrum <nickormy@gmail.com>
2025-03-06 20:26:31 -0300

    fix: Fixes destination email and api email value

--- backend/src/main/java/com/mosaicai/api/Constants/Constants.java
+++ backend/src/main/java/com/mosaicai/api/Constants/Constants.java
@@ -2,8 +2,8 @@ package com.mosaicai.api.Constants;
 
 public class Constants {
     // TODO CHANGE EMAIL HERE
-    public static final String DESTINATION_EMAIL = "productowner@email.com";
-    public static final String APIEMAIL = "ouremail@mosaic.com";
+    public static final String DESTINATION_EMAIL = "flaviamarianeves@gmail.com";
+    public static final String APIEMAIL = "flaviamarianeves@gmail.com";
     public static final String APIEMAILPASKEY = "passkeyfromgoogle";
     public static final String HEADEREMAIL = "https://i.imgur.com/6KmtKmc.png";
 }
