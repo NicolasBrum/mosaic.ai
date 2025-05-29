5e0442bb62408364496fe36fd89de9aee4d9de1f
Nicolas Brum <105520865+NicolasBrum@users.noreply.github.com>
2025-05-29 18:05:30 -0300

    Update Constants.java

--- backend/src/main/java/com/mosaicai/api/Constants/Constants.java
+++ backend/src/main/java/com/mosaicai/api/Constants/Constants.java
@@ -2,8 +2,8 @@ package com.mosaicai.api.Constants;
 
 public class Constants {
     // TODO CHANGE EMAIL HERE
-    public static final String DESTINATION_EMAIL = "flaviamarianeves@gmail.com";
-    public static final String APIEMAIL = "flaviamarianeves@gmail.com";
+    public static final String DESTINATION_EMAIL = "";
+    public static final String APIEMAIL = "";
     public static final String APIEMAILPASKEY = "passkeyfromgoogle";
     public static final String HEADEREMAILPRODUCTOWNER = "https://i.imgur.com/6KmtKmc.png";
     public static final String HEADEREMAILCLIENT = "https://i.imgur.com/z7VqXJL.png";
